cfa94e031d975335691e4677c086a399bb9d9bc4
Co-Su <thisusernameisnottaken@gmail.com>
2017-12-04 16:58:34 -0800

    pseudocoded my woes upon this digital travesty

--- javascript.js
+++ javascript.js
@@ -1,13 +1,14 @@
 
-		
 		var animals = ["red panda","dog","cat","iguana"]
 		
 		function displayAnimalGifs() {
         
         var animal = $(this).attr("data-name");
-		var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=dc6zaTOxFJmzC&limit=5");
-		//dc6zaTOxFJmzC
-		$.ajax({
+		var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + animal + "&rating=pg&api_key=dc6zaTOxFJmzC&limit=5");
+		
+    // API key = dc6zaTOxFJmzC
+		
+    $.ajax({
 			url: queryURL,
 			method: "GET"
 			})
@@ -16,7 +17,7 @@
         	var imageUrl = response.data.image_original_url;
         	var animalImage = $("<img>");
         	animalImage.attr("src", imageUrl);
-        	animalImage.attr("alt", "There should be a GIF here...  :/");
+        	animalImage.attr("alt", "There should be a GIF here...  :(");
         	
         	$("#gif-view").append(animalImage);
       		
@@ -25,7 +26,7 @@
       	
       	function renderButtons() {
         $("#buttons-view").empty();
-		//loop through animal array
+		    //loop through animal array
         for (var i = 0; i < animals.length; i++) {
         //make a var for buttons, then dynamically add info to that var, yo.
         var a = $("<button>");
@@ -42,3 +43,13 @@
 		//calls for renderButtons and displayAnimalGifs go here, on the outermost scope.
 		renderButtons();
 		$("button").click(displayAnimalGifs());
+
+    //ideally, this should work, even after cross-referencing with previous assignments.  Not sure why it isn't, to be honest.
+    
+    //until these issues are fixed, this page will be missing the following:
+    
+    //-multiple still gifs
+    
+    //-the ability to pause and play gifs
+
+    //-the ability to add new buttons, allowing the user to search for their own content.
